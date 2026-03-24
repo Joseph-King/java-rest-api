38a3d03e4081c6dfc461507f04fb63a8878c1556
Joseph King <josephck1997@gmail.com>
2026-03-24 19:19:54 -0400

    Included post and upload test

---
+++ tests/test-post.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:8080/test/post-json \
+-H "Content-Type: application/json" \
+-d '{"message": "Hello World"}'
---
+++ tests/test-upload.sh
@@ -0,0 +1,2 @@
+curl -X POST http://localhost:8080/test/upload-txt \
+-F "file=@./files/test.txt"
